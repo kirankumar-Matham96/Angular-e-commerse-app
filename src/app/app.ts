2bc6621d5bf590a16888f4a6650c93ea03058d0f
Kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2026-02-15 18:14:12 +0530

    add: hero section bg image

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
+import { Hero } from './hero/hero';
 import { Footer } from './footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer],
+  imports: [RouterOutlet, Header, Hero, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
