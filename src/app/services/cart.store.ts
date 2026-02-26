5750964bc61d9bc93f529f6f1f2e8f8c954c2689
Kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2026-02-26 14:50:33 +0530

    add: cart store with rxjs state

---
+++ src/app/services/cart.store.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+export interface CartItem {
+  id: number;
+  title: string;
+  price: number;
+  quantity: number;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CartStore {
+  // cartSubject to hold the cart state
+  private cartSubject = new BehaviorSubject<CartItem[]>([]);
+  cart = this.cartSubject.asObservable();
+
+  getCartValue() {
+    return this.cartSubject.getValue(); // gets the cart values
+  }
+
+  addToCart(item: CartItem) {
+    // get all values
+    const currentCart = this.getCartValue();
+
+    // find if exists
+    const existingItem = currentCart.find((product) => product.id === item.id);
+
+    if (existingItem) {
+      existingItem.quantity += 1; // if exists, increase the quantity
+    } else {
+      currentCart.push({ ...item, quantity: 1 }); // if not add quantity property with value 1
+    }
+
+    // update the state
+    this.cartSubject.next([...currentCart]);
+  }
+
+  removeFromCart(id: number) {
+    // filter out the item from the cart state
+    const updatedCart = this.getCartValue().filter((product) => product.id != id);
+
+    // update the new state
+    this.cartSubject.next(updatedCart);
+  }
+
+  clearCart() {
+    // update the cart state with empty array
+    this.cartSubject.next([]);
+  }
+}
