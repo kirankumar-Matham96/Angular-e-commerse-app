7e9fc104a15bed8b1c6a3eb7d1a0743ecd31fc15
Kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2026-02-26 15:38:37 +0530

    add: cart items number showed in header

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CartStore } from '../services/cart.store';
+import { map, Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
+
 @Component({
   selector: 'app-header',
-  imports: [RouterLink],
+  imports: [AsyncPipe, RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
-export class Header {}
+export class Header {
+  cartItemsCount: Observable<number>;
+
+  constructor(private cartStore: CartStore) {
+    this.cartItemsCount = this.cartStore.cart.pipe(
+      map((items) => items.reduce((total, item) => total + item.quantity, 0)),
+    );
+    console.log({ cartItemsCount: this.cartItemsCount });
+  }
+}
